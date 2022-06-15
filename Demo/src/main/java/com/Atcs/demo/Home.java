c00ebcb8f0f2920779d79d1afae464fe330b2b02
Jatin Nama <jatin.nama@atcs.com>
2022-06-15 13:06:35 +0530

    Submit

---
+++ Demo/src/main/java/com/Atcs/demo/Home.java
@@ -0,0 +1,75 @@
+package com.Atcs.demo;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Home
+ */
+@WebServlet("/Home")
+public class Home extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Home() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("I am in Home(Do Get)");
+		
+		String name=request.getParameter("name");
+		String age=request.getParameter("age");
+		//String rollno=request.getParameter("rollno");
+		
+		System.out.println("name "+name);
+		System.out.println("age "+age);
+		//System.out.println("rollno "+rollno);
+		
+		//use to navigate to new page
+		
+		RequestDispatcher rd=request.getRequestDispatcher("/Home.jsp");
+		request.setAttribute("key1", name);
+		request.setAttribute("key2", age);
+		//request.setAttribute("key3", rollno);
+		rd.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("Home(DoPost");
+		doGet(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPut(HttpServletRequest, HttpServletResponse)
+	 */
+	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("Home(DoPut");
+	}
+
+	/**
+	 * @see HttpServlet#doDelete(HttpServletRequest, HttpServletResponse)
+	 */
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
